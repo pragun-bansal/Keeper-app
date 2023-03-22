c299542ee0630008b567223c4e03e9c657c6560c
pragun-bansal <97455793+pragun-bansal@users.noreply.github.com>
2023-03-22 19:20:18 +0530

    first commit to keeper app

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [isFullRendered, setFullRendered] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    setFullRendered(false);
+    event.preventDefault();
+  }
+
+  function switchRender() {
+    setFullRendered(true);
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isFullRendered && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={switchRender}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isFullRendered ? "3" : "1"}
+        />
+        <Zoom in={isFullRendered}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
